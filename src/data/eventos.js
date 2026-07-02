41d65d82a107e9aea212b7fb7c7f7c18702f1efd
Facundo Almiento <Facundoalmiento@gmail.com>
2026-07-02 12:56:27 -0300

    travesia nueva

--- src/data/eventos.js
+++ src/data/eventos.js
@@ -5,15 +5,15 @@ export const eventos = [
     fecha: "7-8-9 Agosto 2026",
     ubicacion: "Pinamar",
     cupos: "Disponibles",
-    inscripcion: "abierta"
+    inscripcion: "cerrada"
   },
 
   {
-    id: "barro-2026",
-    titulo: "Barro Extremo",
-    fecha: "15 Septiembre 2026",
-    ubicacion: "Canteras",
-    cupos: "Ultimos lugares",
-    inscripcion: "preparacion"
+    id: "cordoba-san-luis-2026",
+    titulo: "Cruce de Sierras - Pueblo escondido",
+    fecha: "proximamente fecha",
+    ubicacion: "De Córdoba a San Luis.",
+    cupos: "Disponibles",
+    inscripcion: "abierta"
   }
 ]
